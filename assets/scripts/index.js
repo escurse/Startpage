015f3e484bee0c600a9157d65c805b8975d4f2af
escurse <av1967@naver.com>
2024-10-10 14:01:06 +0900

    Startpage

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,10 +7,14 @@
         './assets/images/bg/4.jpg',
         './assets/images/bg/5.jpg'
     ];
-    for (const image of images) {
+    for (let i = 0; i < images.length; i++) {
+        const image = images[i];
         const $item = document.createElement('div');
         $item.classList.add('item');
         $item.style.backgroundImage = `url("${image}")`;
+        if (i === 0) {
+            $item.classList.add('--visible');
+        }
         $bg.append($item);
     }
 }
